11f48d4feb91f4ca4f01e52fbdfb405c81537d00
saviopaasiodaspoidoik <112134396+savioaaaaaa@users.noreply.github.com>
2022-09-08 23:21:15 +0000

    i begin wreiting the questons

---
+++ main.py
@@ -0,0 +1,11 @@
+answer=input("you will name the home country of each car manufacturer, it will get harder every question and will be multi answer questions... press A to start")
+if answer.lower() == "a":
+  answer = "a"
+
+print=input("Question number one, which country is citrons home country")
+
+answer=input
+
+
+
+
