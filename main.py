9ea5d23e89a8a739bef1086e8557a5c854493f52
saviopaasiodaspoidoik <112134396+savioaaaaaa@users.noreply.github.com>
2022-09-13 21:39:06 +0000

    i coded the first questions code about the points and did the clear os system

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
+import os
+def clear():
+  os.system('clear')
 answer=input("you will name the home country of each car manufacturer, it will get harder every question and will be multi answer questions... press A to start")
+clear()
+
+points = 0
+
 if answer.lower() == "a":
   answer = "a"
 
-print=input("Question number one, which country is citrons home country")
 
-answer=input
+print("Question number One, \nWhich country is citron's home country?")
+
+print("The possible answers are,  \nA=Belgium\nB=France\nC=Japan\nD=China")
+answer=input()
+
+global points
+
+if answer1 == 'a':
+    points -= 0
+      print('This answer is false, your points: {}'.format(points))
+
+elif answer1 == 'b':
+      
+      points += 1
+      print('This answer is correct, your points: {}'.format(points))
+
+elif answer1 == 'c':
+      
+      points -= 0
+      print('This answer is false, your points: {}'.format(points))
+
+elif answer1 == 'd':
+      
+      points -= 0
+      print('This answer is false, your points: {}'.format(points))
+
+clear()
+
+print("Question number two, \nWhich country is ferrari's home country?")
 
 
 
 
+
+
+
+
+
+#check examplar for help on how to do the answers do the code to remember what to check and make sure to get the code from thje exemplar
+#1. how to make score board
+#2. how to make multi answers the only answers that wprk
